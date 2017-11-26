86175e4c92da71888171023f2e880840184b1222
Ilnur Akhmetov <axmetret@gmail.com>
2017-11-26 11:14:45 +0300

    My first program. This program defines the version of the system, SDK, and so on. /* r00_Hx */ Update: Version OS SDK Model Manufacturer Brand ID Fingerprint CPU ABI

--- app/src/main/java/ru/axmet/vesionphone/MainActivity.java
+++ app/src/main/java/ru/axmet/vesionphone/MainActivity.java
@@ -3,7 +3,6 @@ package ru.axmet.vesionphone;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 
