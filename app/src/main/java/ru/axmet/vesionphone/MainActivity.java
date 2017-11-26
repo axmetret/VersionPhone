564a532c213981824c6733e462636a55d5fbffa6
Ilnur Akhmetov <axmetret@gmail.com>
2017-11-26 11:59:11 +0300

    My first program. This program defines the version of the system, SDK, and so on. /* nickname: r00_Hx e-mail address: axmetret@gmail.com */ Update: Version OS - Various version strings. SDK - The user-visible SDK version of the framework; its possible values are defined in Build.VERSION_CODES. Model - The end-user-visible name for the end product. Manufacturer - The manufacturer of the product/hardware. Brand - The consumer-visible brand with which the product/hardware will be associated, if any. ID - A string that uniquely identifies this build. Do not attempt to parse this value. Fingerprint - A string that uniquely identifies this build. Do not attempt to parse this value. CPU ABI - The name of the instruction set (CPU type + ABI convention) of native code. \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Update 2: BOOTLOADER - The system bootloader version number. BOARD - The name of the underlying board, like "goldfish". DEVICE - The name of the industrial design. DISPALY - A build ID string meant for displaying to the user. HARDWARE - The name of the hardware (from the kernel command line or /proc). HOST - ??? TAGS - Comma-separated tags describing the build, like "unsigned,debug". TIME - ??? TYPE - The type of build, like "user" or "eng". USER - ??? RADIO - Returns the version string for the radio firmware.

--- app/src/main/java/ru/axmet/vesionphone/MainActivity.java
+++ app/src/main/java/ru/axmet/vesionphone/MainActivity.java
@@ -17,25 +17,59 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("Version OS:" + " " +  Build.VERSION.RELEASE);
 
         TextView textView2 = (TextView) findViewById(R.id.textView2);
-        textView2.setText("Build SDK:" + " " + Build.VERSION.SDK_INT);
+        textView2.setText("SDK:" + " " + Build.VERSION.SDK_INT);
 
         TextView textView3 = (TextView) findViewById(R.id.textView3);
-        textView3.setText("Build Model:" + " " + Build.MODEL);
+        textView3.setText("Model:" + " " + Build.MODEL);
 
         TextView textView4 = (TextView) findViewById(R.id.textView4);
-        textView4.setText("Build Manufacturer:" + " " + Build.MANUFACTURER );
+        textView4.setText("Manufacturer:" + " " + Build.MANUFACTURER );
 
         TextView textView5 = (TextView) findViewById(R.id.textView5);
-        textView5.setText("Build Brand:" + " " + Build.BRAND);
+        textView5.setText("Brand:" + " " + Build.BRAND);
 
         TextView textView6 = (TextView) findViewById(R.id.textView6);
-        textView6.setText("Build ID:" + " " + Build.ID);
+        textView6.setText("ID:" + " " + Build.ID);
 
         TextView textView7 = (TextView) findViewById(R.id.textView7);
-        textView7.setText("Build Fingerprint:" + " " + Build.FINGERPRINT);
+        textView7.setText("Fingerprint:" + " " + Build.FINGERPRINT);
 
         TextView textView8 = (TextView) findViewById(R.id.textView8);
         textView8.setText("CPU ABI:" + " " + Build.CPU_ABI);
+
+        TextView textView9 = (TextView) findViewById(R.id.textView9);
+        textView9.setText("BOOTLOADER:" + " " + Build.BOOTLOADER);
+
+        TextView textView10 = (TextView) findViewById(R.id.textView10);
+        textView10.setText("BOARD:" + " " + Build.BOARD);
+
+        TextView textView11 = (TextView) findViewById(R.id.textView11);
+        textView11.setText("DEVICE:" + " " + Build.DEVICE);
+
+        TextView textView12 = (TextView) findViewById(R.id.textView12);
+        textView12.setText("DISPLAY:" + " " + Build.DISPLAY);
+
+        TextView textView13 = (TextView) findViewById(R.id.textView13);
+        textView13.setText("HARDWARE:" + " " + Build.HARDWARE);
+
+        TextView textView14 = (TextView) findViewById(R.id.textView14);
+        textView14.setText("HOST:" + " " + Build.HOST);
+
+        TextView textView15 = (TextView) findViewById(R.id.textView15);
+        textView15.setText("TAGS:" + " " + Build.TAGS);
+
+        TextView textView16 = (TextView) findViewById(R.id.textView16);
+        textView16.setText("TIME:" + " " + Build.TIME);
+
+        TextView textView17 = (TextView) findViewById(R.id.textView17);
+        textView17.setText("TYPE:" + " " + Build.TYPE);
+
+        TextView textView18 = (TextView) findViewById(R.id.textView18);
+        textView18.setText("USER:" + " " + Build.USER);
+
+        TextView textView19 = (TextView) findViewById(R.id.textView19);
+        textView19.setText("RADIO:" + " " + Build.getRadioVersion());
+
 
     }
 }
